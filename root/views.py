be6c65098506e5ddc0c2ea5c6caafcf04aa75499
Nikhil-Gupta76 <nikhilgupta8595665@gmail.com>
2026-02-22 10:13:49 +0530

    Added the view of the root

--- root/views.py
+++ root/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from root.models import Url
+
 
 def index(request):
     if request.method == "POST":
         full_url = request.POST.get('full_url')
+
         if full_url:
             obj = Url.create(full_url)
+
             if obj:
                 return render(request, 'index.html', {
                     'full_url': obj.full_url,
                     'short_url': obj.short_url
                 })
-            else:
-                return HttpResponse("Error creating URL")
-        else:
-            return HttpResponse("No URL provided")
+
+        return HttpResponse("Error")
+
     return render(request, 'index.html')
+
+
+# NEW: Redirect view
+def redirect_url(request, short_url):
+    obj = get_object_or_404(Url, short_url=short_url)
+    return redirect(obj.full_url)
