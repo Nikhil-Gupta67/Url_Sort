1f43adea8ddd20d01b99ff36b3c6515c1e59ed5e
Nikhil-Gupta76 <nikhilgupta8595665@gmail.com>
2026-03-12 21:53:42 +0530

    Updated the url

--- url_sort/urls.py
+++ url_sort/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from root.views import index
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('root.urls')),
